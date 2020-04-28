1d93c6562b21b1cb6a54c6b56d02e68d43b271b6
root <root@jammck19801c.mylabserver.com>
2020-04-28 10:49:36 +0000

    Refactored Install Class for Debain family members

---
+++ manifests/params.pp
@@ -0,0 +1,13 @@
+# @summary for operating system variables
+#
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': { 
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
